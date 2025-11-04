da5bdf2b9789606eaf40d84bfa00d994dd0f4c24
Trupti-Ranjan <adarshbarik563@gmail.com>
2025-11-04 11:26:47 +0530

    Initialized the redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import {login, logout} from "./store/authSlice.js"
+import {Header, Footer} from "./components"
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false)) // it always run and we process all the data like if the user is loggedin or not and then we change the loading state to false 
+  }, [])
   return (
     <>
-      <h1>Blog with Appwrite</h1>
+      {!loading? <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+        <div className="w-full block">
+          <Header />
+          <main>
+            {/* <Outlet /> */}
+          </main>
+          <Footer />
+        </div>
+      </div> : null}
     </>
   );
 }
